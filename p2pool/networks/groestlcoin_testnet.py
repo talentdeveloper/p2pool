00f0f368a9dca67ac481bdcbd936c35c16e844c0
Gruve_p <gruve-p@users.noreply.github.com>
2018-02-12 19:10:10 +0100

    Updated Testnet Bootstrap address

--- p2pool/networks/groestlcoin_testnet.py
+++ p2pool/networks/groestlcoin_testnet.py
@@ -13,8 +13,8 @@ MIN_TARGET = 0
 MAX_TARGET = 2**256//2**20 - 1
 PERSIST = False
 WORKER_PORT = 21330
-BOOTSTRAP_ADDRS = ''.split(' ')
-ANNOUNCE_CHANNEL = ''
+BOOTSTRAP_ADDRS = 'testp2pool.groestlcoin.org'.split(' ')
+ANNOUNCE_CHANNEL = '#groestlcoin'
 VERSION_CHECK = lambda v: None if 2130300 <= v else 'Groestlcoin version too old. Upgrade to 2.13.3 or newer!'
 VERSION_WARNING = lambda v: None
 SOFTFORKS_REQUIRED = set(['bip65', 'csv', 'segwit'])
